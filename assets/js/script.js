000278062150b8b5650e8e5e0128b766d0b4f113
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-04-12 14:35:40 -0700

    making sure cur stage is in place - dark mode and contact page to go

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 $('.dropdown-trigger').dropdown();
 $('.fixed-action-btn').floatingActionButton();
+
+$('.dark-toggle').on('click',function(){
+  alert('clicked');
+  if ($(this).find('i').text() == 'brightness_4'){
+          $(this).find('i').text('brightness_high');
+  } else {
+          $(this).find('i').text('brightness_4');
+  }
+});
