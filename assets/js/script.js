0ee58fcc57df047b11af21d1c33d80c3a36da55a
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-04-25 13:47:29 -0700

    fixing layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
 $('.dropdown-trigger').dropdown();
 $('.fixed-action-btn').floatingActionButton();
-
-$('.dark-toggle').on('click',function(){
-  alert('clicked');
-  if ($(this).find('i').text() == 'brightness_4'){
-          $(this).find('i').text('brightness_high');
-  } else {
-          $(this).find('i').text('brightness_4');
-  }
-});
+$('.collapsible').collapsible();
